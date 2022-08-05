8eca2b16490ab25a6541ee98a42dc6bbcfc025b6
Ericjr688 <ericasare688@gmail.com>
2022-08-05 13:30:52 -0400

    sets state using profile information

--- src/Pages/ProfileView.js
+++ src/Pages/ProfileView.js
@@ -19,6 +19,7 @@ import { useCollectionData } from 'react-firebase-hooks/firestore'
 
 export default function ProfileView(props) {
   let { username } = useParams();
+  const [ prodileData, setProfileData ] = useState({});
 
 
   const userEmail = props.userEmail;
@@ -27,9 +28,18 @@ export default function ProfileView(props) {
     console.log('ref', ref)
     const docSnap = await getDoc(ref);
     console.log(docSnap.data())
-    // create states for profile data and assign
+    setProfileData({
+      name: docSnap.data().name,
+      email: docSnap.data().email,
+      profilePicUrl: docSnap.data().profilePicUrl,
+      followers: docSnap.data().followers,
+      following: docSnap.data().following,
+      photos: docSnap.data().photos,
+      posts: docSnap.data().posts,
+    })
   }
 
+  // use effect?
   getUserFromEmail()
 
   // finds user information from firebase and displays it
