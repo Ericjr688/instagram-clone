ee5951a9dabb506fd9cc123cea2244176d043b88
Ericjr688 <ericasare688@gmail.com>
2022-08-11 02:52:20 -0400

    improved method of getting post image url

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
   getStorage,
   ref,
@@ -8,15 +8,19 @@ import {
 } from 'firebase/storage';
 
 export default function Post(props) {
+  const [ postPic, setPostPic ] = useState();
   const storage= getStorage();
 
   async function getPostImageUrl() {
-    let url = await getDownloadURL(ref(storage, props.postData.picDirectory))
-    return url;
+    try {
+      let result = await getDownloadURL(ref(storage, props.postData.picDirectory))
+      setPostPic(result)
+    } catch (error) {
+      console.error('Following error with downloading post url: ', error)
+    }
   }
 
-  const postPicUrl = getPostImageUrl();
-
+  getPostImageUrl();
   return (
     <div className='post'>
       <div className='post-header'>
@@ -24,7 +28,7 @@ export default function Post(props) {
         <span>{props.postData.name}</span>
       </div>
       <div className='post-image'>
-      <img src={postPicUrl} alt='post pic'></img>
+        <img src={postPic} alt='post pic' className='responsive-img'></img>
       </div>
       <div className='post-content'>
         <div className='likes'> {props.postData.likes} likes</div>
